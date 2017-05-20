2c4cc8531a843da08590a965b61a2e1d2a660246
Euan Mackintosh <student@macbook.local>
2017-05-20 16:52:36 -0400

    Finished The Velvet Underground

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
@@ -23,8 +23,76 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
 
-// Your code below...
+canvas.drawShapesWithBorders = false
 
-//: ## Template code
-//: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
-PlaygroundPage.current.liveView = canvas.imageView
+canvas.fillColor = Color.black
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
+
+func drawArrow(X: Int, Y: Int) {
+    canvas.translate(byX: X, byY: Y)
+    
+    //Draw the vertical and horisonatal part of the arrow
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 15, height: 45)
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 45, height: 15)
+    
+    //Draw the stick of the arrow
+    canvas.rotate(by: 45)
+    canvas.drawRectangle(bottomLeftX: 20, bottomLeftY: -8, width: 50, height: 15)
+    canvas.rotate(by: -45)
+    
+    
+    canvas.translate(byX: -X, byY: -Y)
+}
+
+
+for x in stride(from: 0, through: 335, by: 67) {
+    for y in stride(from: 0, through: 335, by: 67) {
+        
+        //Only on the diagonal is this true
+        if x == y  {
+            //Make it white
+            canvas.fillColor = Color.white
+            drawArrow(X: x, Y: y)
+
+        } else {
+            //Leave it as purple
+        canvas.fillColor = Color.init(hue: 313, saturation: 77, brightness: 65, alpha: 100)
+        
+            drawArrow(X: x, Y: y)
+            
+        }
+    }
+
+}
+
+//Set the line color
+canvas.fillColor = Color.white
+
+//Set the text color
+canvas.textColor = Color.white
+
+//Title
+canvas.drawText(message: "the velvet underground", size: 30, x: 15, y: 400)
+
+//Draw bottom White line accross the canvas
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 440, width: 450, height: 2)
+
+//Draw the first box of text
+canvas.drawText(message: "first appearance in london", size: 9, x: 10, y: 535)
+canvas.drawText(message: "the london college of printing", size: 9, x: 10, y: 525)
+
+//Draw the second box of text
+canvas.drawText(message: "with spring and pollyfloskin", size: 9, x: 150, y: 535)
+canvas.drawText(message: "plus the great western light show", size: 9, x: 150, y: 525)
+
+//Draw the third box of text
+canvas.drawText(message: "thursday", size: 9, x: 300, y: 535)
+canvas.drawText(message: "october 14 1981/8 pm", size: 9, x: 300, y: 525)
+
+//Draw the second white line accross the canvas
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 560, width: 400, height: 2)
+
+
+    //: ## Template code
+    //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
+    PlaygroundPage.current.liveView = canvas.imageView
