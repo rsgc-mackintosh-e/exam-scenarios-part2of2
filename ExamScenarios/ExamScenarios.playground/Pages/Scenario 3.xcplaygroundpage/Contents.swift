6fa3de969a79ef2070721ba69f8987700b38ac51
Euan Mackintosh <student@macbook.local>
2017-05-21 17:13:41 -0400

    Finished Velocity Girl

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 3.xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 3.xcplaygroundpage/Contents.swift
@@ -22,6 +22,43 @@ import PlaygroundSupport
 //: ## Your code starts here
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
+canvas.drawShapesWithBorders = false
+canvas.drawShapesWithFill = true
+//drawing the big triangles
+for Tricolor in 1...2{
+    var x = 0
+    if Tricolor == 1{
+        canvas.fillColor = Color.yellow
+        x = 0
+    }else{
+        canvas.fillColor = Color.white
+        x = 400
+    }
+    var triangles : [NSPoint] = []
+    triangles.append( NSPoint( x: -1 , y: 200))
+    triangles.append( NSPoint( x: 400-x , y: 200+x))
+    triangles.append( NSPoint( x: 400 , y: 601))
+    canvas.drawCustomShape(with: triangles)
+}
+
+//rectangle on the ground
+canvas.fillColor = Color(hue: 10, saturation: 85, brightness: 97, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 200)
+
+//small trangles
+canvas.fillColor = Color(hue: 10, saturation: 85, brightness: 97, alpha: 100)
+for xAxis in stride(from: 0, to: 9, by: 1){
+    for yAxis in stride(from: 0, to: 9, by: 1){
+        var side = ((405/9) * xAxis) - 1
+        var up = (405/9) * yAxis
+        var Wvertices : [NSPoint] = []
+        Wvertices.append( NSPoint( x: side , y: 200 + up))
+        Wvertices.append( NSPoint( x: side , y: 200 + up + (405/9)))
+        Wvertices.append( NSPoint( x: side + (405/9) , y: 200 + up + (405/9)))
+        canvas.drawCustomShape(with: Wvertices)
+    }
+}
+
 
 
 //: ## Template code
--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 4 .xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 4 .xcplaygroundpage/Contents.swift
@@ -25,13 +25,72 @@ let canvas = Canvas(width: 400, height: 600)
 
 // Color constants
 let offWhite = Color(hue: 87, saturation: 6, brightness: 88, alpha: 100)
+
 let yellow = Color(hue: 37, saturation: 93, brightness: 98, alpha: 100)
+
 let pink = Color(hue: 326, saturation: 99, brightness: 93, alpha: 100)
+
 let blue = Color(hue: 240, saturation: 80, brightness: 50, alpha: 50)
+
 let black = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
 
 // Your code below...
+canvas.fillColor = Color(hue: 87, saturation: 6, brightness: 88, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 400, height: 600)
 
+
+canvas.drawShapesWithBorders = false
+
+
+for x in stride(from: -50, to: 450, by: 100){
+    for y in stride(from: -75, to: 425, by: 100) {
+        
+        
+        if y - x == -25 || x + y == 275 && x > 200 {
+            canvas.fillColor = Color.init(hue: 37, saturation: 93, brightness: 98, alpha: 100)
+        }
+            
+        else {
+            canvas.fillColor = Color.init(hue: 326, saturation: 99, brightness: 93, alpha: 100)
+        }
+        canvas.drawRoundedRectangle(bottomLeftX: x, bottomLeftY: y, width: 95, height: 95)
+        
+        
+        
+    }
+}
+
+for x in stride(from: -75, to: 425, by: 100){
+    for y in stride(from: 0, to: 400, by: 100) {
+        
+        
+        canvas.fillColor = Color.init(hue: 240, saturation: 80, brightness: 50, alpha: 50)
+        
+        canvas.drawRoundedRectangle(bottomLeftX: x, bottomLeftY: y, width: 95, height: 95)
+        
+        
+        
+    }
+}
+
+//Write Velocity Girls
+canvas.drawText(message: "velocity girl", size: 45, x: 10 , y: 420)
+
+
+//Stupid info stuf at the top uif the dokument
+
+
+//Stuf beside "Velocity Girl"
+canvas.drawText(message: "with special guest", size: 12, x: 275, y: 445)
+canvas.drawText(message: "sunny day real estate", size: 12, x: 275 , y: 430)
+
+//draw left info
+canvas.drawText(message: "at the tavern", size: 9, x: 10, y: 500)
+canvas.drawText(message: "american university / washington, dc", size: 9, x: 10, y: 490)
+
+//draw top right info
+canvas.drawText(message: "friday", size: 9, x: 275, y: 570)
+canvas.drawText(message: "september 23 1994 / 8pm", size: 9, x: 275, y: 560)
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
